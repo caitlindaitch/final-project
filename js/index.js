bb9e7d818965f1a44934017b0516a8cb7022fb79
caitlindaitch <caitlin.daitch@agencyq.com>
2015-08-24 19:08:45 -0400

    Added a form, animation to popup. Still need to connect form to email.

--- js/index.js
+++ js/index.js
@@ -1,10 +1,25 @@
 $(document).ready(function() {
 
-  $('a').click(function(){
+  $('.scroll-down').click(function(){
       $('html, body').animate({
           scrollTop: $( $.attr(this, 'href') ).offset().top
       }, 1200);
       return false;
   });
+
+  function showPopup() {
+    $(".popup").fadeIn();
+
+    $("body").addClass("overlay-background");
+  }
+
+  function hidePopup () {
+    $(".popup").fadeOut();
+
+    $("body").removeClass("overlay-background")
+  }
+
+  $(".buy-now").click(showPopup);
+  $(".close-icon").click(hidePopup);
 
 });
